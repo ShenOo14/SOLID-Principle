903c17066338c712e71347857dba022430cd3ca4
ShenOo14 <mostafa2023shnawany@gmail.com>
2024-12-22 23:32:05 +0200

    Add files via upload

---
+++ open&closed.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+def calculate_area(shapes):
+    for shape in shapes:
+        print(f'The area is: {shape.area()}')
+
+shapes = [Rectangle(10, 20), Circle(5)]
+calculate_area(shapes)
