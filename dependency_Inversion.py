6ec57b0d9730b08b34fa60961b8ca7905a0dca00
ShenOo14 <mostafa2023shnawany@gmail.com>
2024-12-26 16:36:19 +0200

    Update dependency_Inversion.py

--- dependency_Inversion.py
+++ dependency_Inversion.py
@@ -1,29 +1,32 @@
+#acheiving dependency inversion principle
 #we implement more devices on one switch
-class Switch:
+class Switch:  
     def __init__(self, device):
         self.device = device
-       
+        
+class KeyStatus(Switch):
+
     def turn_on(self):
         print(f"{self.device} is Switched on")
     def turn_off(self):
         print(f"{self.device} is Switched off")
         
         
-device1 = Switch("television")
+device1 = KeyStatus("television")
 device1.turn_on()
 device1.turn_off()
 device1.turn_on()
 
 print('-----------------')
 
-device2 = Switch("fan")
+device2 = KeyStatus("fan")
 device2.turn_on()
 device2.turn_off()
 device2.turn_on()
 
 print('-----------------')
 
-device3 = Switch("electric lamp")
+device3 = KeyStatus("electric lamp")
 device3.turn_on()
 device3.turn_off()
 device3.turn_on()
