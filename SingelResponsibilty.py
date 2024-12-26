0b5705c85ef57ac08033bc4805243e1da3271de1
ShenOo14 <mostafa2023shnawany@gmail.com>
2024-12-26 16:03:36 +0200

    Update SingelResponsibilty.py

--- SingelResponsibilty.py
+++ SingelResponsibilty.py
@@ -1,3 +1,24 @@
+#violate the single responsibility principle
+class Report:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+    def get_title(self):
+        return self.title
+
+    def get_content(self):
+        return self.content
+
+    def print_report(self):
+        print(f"Title: {self.get_title()}")
+        print(f"Content: {self.get_content()}")
+
+
+report = Report("Monthly Report", " content for the monthly report.")
+report.print_report()
+
+#achieving single responsibility principle
 class Report:
     def __init__(self, title, content):
         self.title = title
@@ -19,7 +40,7 @@ class ReportPrinter:
         print(f"Content: {self.report.get_content()}")
 
 
-# Example usage
-report = Report("Monthly Report", "This is the content of the monthly report.")
+
+report = Report("Monthly Report", " content for the monthly report.")
 printer = ReportPrinter(report)
 printer.print_report()
